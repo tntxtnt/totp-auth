874d8499b599f727dd30ff702ebc3f065a5967c4
Tri Tran <lepnit205@gmail.com>
2022-05-15 12:38:50 -0500

    Add clang-format and clang-tidy

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,55 @@
 #include "totp.h"
 #include <SFML/Graphics.hpp>
+#include <utility>
 using namespace CryptoPP;
 
 namespace ui
 {
 sf::Font font;
 
-bool init()
-{
-    if (!font.loadFromFile("./res/fonts/RobotoMono-Regular.ttf"))
-        return false;
-    return true;
+auto init() -> bool {
+    return font.loadFromFile("./res/fonts/RobotoMono-Regular.ttf");
 }
 
-class TimeLeftShape : public sf::Shape
-{
+class TimeLeftShape : public sf::Shape {
 public:
-    TimeLeftShape(float radius, size_t maximumPoints = 30,
-                  float percentage = 1.0f)
-    : radius(radius), percentage(percentage), maximumPoints(maximumPoints)
-    {
+    TimeLeftShape(float radius, size_t maximumPoints = 30, float percentage = 1.0F)
+    : radius(radius), percentage(percentage), maximumPoints(maximumPoints) {
         update();
     }
-    void setPercentage(float percentage)
-    {
+    void setPercentage(float percentage) {
         this->percentage = percentage;
         update();
     }
 
 private:
-    size_t getPointCount() const override
-    {
-        return (size_t)ceil(maximumPoints * percentage) + 1;
+    auto getPointCount() const -> size_t override {
+        return static_cast<size_t>(std::ceil(maximumPoints * percentage)) + 1;
     }
-    sf::Vector2f getPoint(size_t index) const override
-    {
-        static const float tau = 6.283185307f;
-        if (index == 0)
-            return sf::Vector2f(radius, radius);
+    auto getPoint(size_t index) const -> sf::Vector2f override {
+        static const float tau = 6.283185307F;
+        if (index == 0) return {radius, radius};
         float angle = --index * tau / maximumPoints - tau / 4;
         float x = -cos(angle) * radius;
         float y = sin(angle) * radius;
-        return sf::Vector2f(radius + x, radius + y);
+        return {radius + x, radius + y};
     }
 
-private:
+
     float radius;
     float percentage;
     size_t maximumPoints;
 };
 
-class Row : public sf::Drawable, public sf::Transformable
-{
+class Row : public sf::Drawable, public sf::Transformable {
 public:
-    Row(const std::string& name, const std::string& base32Key)
+    Row(const std::string& name, std::string base32Key)
     : label(name, font, 10),
-      base32Key(base32Key),
+      base32Key(std::move(base32Key)),
       code("Invalid key", font, 24),
-      timeLeft(5.5f, 100),
+      timeLeft(5.5F, 100),
       timeLeftPercentage(0),
-      background(sf::Vector2f(240, 80))
-    {
+      background(sf::Vector2f(240, 80)) {
         background.setFillColor(sf::Color::White);
         code.move(20, 15);
         code.setFillColor(sf::Color(0x00, 0x59, 0xcf));
@@ -69,13 +58,10 @@ public:
         timeLeft.move(code.getPosition() + sf::Vector2f(190, 39));
         timeLeft.setFillColor(code.getFillColor());
     }
-    void update()
-    {
+    void update() {
         float timeLeftPercentage = (float)totp::timeLeft().count() / 30;
-        if (timeLeftPercentage > this->timeLeftPercentage)
-        {
-            if (auto code = totp::googleAuthenticatorCode(base32Key))
-            {
+        if (timeLeftPercentage > this->timeLeftPercentage) {
+            if (auto code = totp::googleAuthenticatorCode(base32Key)) {
                 auto codeStr = *code;
                 codeStr.insert(3, 1, ' ');
                 this->code.setString(codeStr);
@@ -83,11 +69,10 @@ public:
         }
         timeLeft.setPercentage(this->timeLeftPercentage = timeLeftPercentage);
     }
-    std::string getName() const { return label.getString().toAnsiString(); }
+    auto getName() const -> std::string { return label.getString().toAnsiString(); }
 
 private:
-    void draw(sf::RenderTarget& target, sf::RenderStates states) const override
-    {
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
         states.transform *= getTransform();
 
         target.draw(background, states);
@@ -96,7 +81,7 @@ private:
         target.draw(timeLeft, states);
     }
 
-private:
+
     sf::Text label;
     std::string base32Key;
     mutable sf::Text code;
@@ -105,62 +90,45 @@ private:
     sf::RectangleShape background;
 };
 
-class Rows : public sf::Drawable
-{
+class Rows : public sf::Drawable {
 public:
-    void append(const std::string& name, const std::string& base32Key)
-    {
+    void append(const std::string& name, const std::string& base32Key) {
         rows.emplace_back(found(name) ? rename(name) : name, base32Key);
         // Move new row to last row position
-        if (rows.size() > 1)
-            rows.back().move(end(rows)[-2].getPosition());
-        rows.back().move(0, 15.f + (rows.size() > 1 ? 80.f : 0));
+        if (rows.size() > 1) rows.back().move(end(rows)[-2].getPosition());
+        rows.back().move(0, 15.F + (rows.size() > 1 ? 80.F : 0));
     }
-    size_t size() const { return rows.size(); }
-    void update()
-    {
-        for (auto& row : rows)
-            row.update();
+    auto size() const -> size_t { return rows.size(); }
+    void update() {
+        for (auto& row : rows) row.update();
     }
 
 private:
-    std::string rename(std::string name) const
-    {
+    auto rename(std::string name) const -> std::string {
         name += " (";
-        for (int i = 1;; ++i)
-        {
+        for (int i = 1;; ++i) {
             std::string newName = name + std::to_string(i) + ")";
-            if (!found(newName))
-                return newName;
+            if (!found(newName)) return newName;
         }
     }
-    bool found(const std::string& name) const
-    {
-        return std::find_if(begin(rows), end(rows), [=](auto& row) {
-                   return row.getName() == name;
-               }) != end(rows);
+    auto found(const std::string& name) const -> bool {
+        return std::find_if(begin(rows), end(rows), [=](auto& row) { return row.getName() == name; }) != end(rows);
     }
-    void draw(sf::RenderTarget& target, sf::RenderStates states) const override
-    {
-        for (auto& row : rows)
-            target.draw(row, states);
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
+        for (const auto& row : rows) target.draw(row, states);
     }
 
-private:
+
     std::vector<ui::Row> rows;
 };
 } // namespace ui
 
-int main()
-{
-    if (!ui::init())
-        return 1;
+auto main() -> int {
+    if (!ui::init()) return 1;
 
     sf::ContextSettings ctxSettings;
     ctxSettings.antialiasingLevel = 8;
-    sf::RenderWindow window({240, 400}, "TOTP Auth",
-                            sf::Style::Titlebar | sf::Style::Close,
-                            ctxSettings);
+    sf::RenderWindow window({240, 400}, "TOTP Auth", sf::Style::Titlebar | sf::Style::Close, ctxSettings);
     window.setFramerateLimit(30);
 
     ui::Rows rows;
@@ -168,12 +136,9 @@ int main()
     rows.append("test key: 2222333344445555", "2222 3333 4444 5555");
     rows.append("test hnry", "hnry ijgf 4htg lxat ixls uirh qe3e 3jtx ");
 
-    while (window.isOpen())
-    {
-        for (sf::Event event; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+    while (window.isOpen()) {
+        for (sf::Event event{}; window.pollEvent(event);) {
+            if (event.type == sf::Event::Closed) window.close();
         }
 
         rows.update();
--- src/totp.h
+++ src/totp.h
@@ -19,21 +19,17 @@ namespace impl
 {
     // Generate HMAC-SHAxxx hash of (key, msg)
     template <class SHA>
-    std::array<byte, SHA::DIGESTSIZE>
-    generateHash(const SecByteBlock& key, const SecByteBlock& msg) noexcept
-    {
+    auto generateHash(const SecByteBlock& key, const SecByteBlock& msg) noexcept -> std::array<byte, SHA::DIGESTSIZE> {
         HMAC<SHA> hmac(key, key.size());
         hmac.Update(msg, msg.size());
-        std::array<byte, SHA::DIGESTSIZE> hash;
+        std::array<byte, SHA::DIGESTSIZE> hash{};
         hmac.Final(&hash[0]);
         return hash;
     }
 
     // Convert a HMAC hash to n-digits passcode string
     template <size_t HASH_LEN>
-    std::string hmacHash2Passcode(const std::array<byte, HASH_LEN>& hash,
-                                  int digits) noexcept
-    {
+    auto hmacHash2Passcode(const std::array<byte, HASH_LEN>& hash, int digits) noexcept -> std::string {
         size_t offset = hash.back() & 0xF;
         // clang-format off
         int binary =
@@ -42,43 +38,33 @@ namespace impl
              ((hash[offset + 2] & 0xFF) << 8) |
              (hash[offset + 3] & 0xFF);
         // clang-format on
-        const int DIGITS_POWER[] = {1,      10,      100,      1000,     10000,
-                                    100000, 1000000, 10000000, 100000000};
+        const int DIGITS_POWER[] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
         int otp = binary % DIGITS_POWER[digits];
         std::string result(digits, '0');
-        for (size_t i = result.size(); i--; otp /= 10)
-            result[i] += otp % 10;
+        for (size_t i = result.size(); i--; otp /= 10) result[i] += otp % 10;
         return result;
     }
 
     // Generate code with input msg
     template <class SHA>
-    std::string generateCode(const SecByteBlock& key, const SecByteBlock& msg,
-                             int digits = 6) noexcept
-    {
+    auto generateCode(const SecByteBlock& key, const SecByteBlock& msg, int digits = 6) noexcept -> std::string {
         return hmacHash2Passcode(generateHash<SHA>(key, msg), digits);
     }
 
     // Generate code with input timestamp
     template <class SHA>
-    std::string generateCode(const SecByteBlock& key, seconds timestamp,
-                             int digits = 6,
-                             seconds timeStep = seconds{30}) noexcept
-    {
+    auto generateCode(const SecByteBlock& key, seconds timestamp, int digits = 6,
+                      seconds timeStep = seconds{30}) noexcept -> std::string {
         auto tc = timestamp / timeStep;
         SecByteBlock msg(sizeof tc);
-        for (size_t i = msg.size(); i--; tc >>= 8)
-            msg[i] = tc & 0xFF;
+        for (size_t i = msg.size(); i--; tc >>= 8) msg[i] = tc & 0xFF;
         return generateCode<SHA>(key, msg, digits);
     }
 
     // RFC 4648 base32 characters: ABCDEFGHIJKLMNOPQRSTUVWXYZ234567
-    std::optional<unsigned> base32CharValue(char c) noexcept
-    {
-        if (isalpha(c))
-            return (c - 1) % 32;
-        if ('2' <= c && c <= '7')
-            return c - 24;
+    auto base32CharValue(char c) noexcept -> std::optional<unsigned> {
+        if (isalpha(c) != 0) return (c - 1) % 32;
+        if ('2' <= c && c <= '7') return c - 24;
         return {};
     }
 
@@ -90,31 +76,27 @@ namespace impl
     //   - '8' to 'B'
     // Other characters that are not in the set
     // "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567" will make the return value empty
-    std::optional<SecByteBlock>
-    googleAuthenticatorBase32Decode(const std::string& base32Str) noexcept
-    {
+    auto googleAuthenticatorBase32Decode(const std::string& base32Str) noexcept -> std::optional<SecByteBlock> {
         SecByteBlock bytes(base32Str.size() * 5 / 8); // truncate size
-        unsigned bits = 0, bitCount = 0, byteIndex = 0;
-        for (char c : base32Str)
-        {
+        unsigned bits = 0;
+        unsigned bitCount = 0;
+        unsigned byteIndex = 0;
+        for (char c : base32Str) {
             // ignore spaces and dash character
-            if (isspace(c) || c == '-')
-                continue;
+            if ((isspace(c) != 0) || c == '-') continue;
             // correct commonly mistyped characters **SILENTLY**
-            if (c == '0')
+            if (c == '0') {
                 c = 'O';
-            else if (c == '1')
+            } else if (c == '1') {
                 c = 'L';
-            else if (c == '8')
+            } else if (c == '8') {
                 c = 'B';
-            if (auto cValue = base32CharValue(c))
-            {
+            }
+            if (auto cValue = base32CharValue(c)) {
                 bits = (bits << 5) | *cValue;
                 bitCount += 5;
-                if (bitCount >= 8)
-                    bytes[byteIndex++] = bits >> (bitCount -= 8);
-            }
-            else // other invalid chars will be treated as error
+                if (bitCount >= 8) bytes[byteIndex++] = bits >> (bitCount -= 8);
+            } else // other invalid chars will be treated as error
                 return {};
         }
         // zero-filled remaining bytes or MSVC will be unhappy
@@ -126,8 +108,7 @@ namespace impl
 // Generate TOTP key with appropriate key length
 // (at least the length of HMAC output)
 template <class SHA>
-SecByteBlock generateKey() noexcept
-{
+auto generateKey() noexcept -> SecByteBlock {
     AutoSeededRandomPool prng;
     SecByteBlock key(SHA::DIGESTSIZE);
     prng.GenerateBlock(key, key.size());
@@ -136,34 +117,27 @@ SecByteBlock generateKey() noexcept
 
 // Generate a time-based one-time passcode
 template <class SHA>
-std::string generateCode(const SecByteBlock& key, int digits = 6,
-                         seconds timeStep = seconds{30}) noexcept
-{
+auto generateCode(const SecByteBlock& key, int digits = 6, seconds timeStep = seconds{30}) noexcept -> std::string {
     auto tm = duration_cast<seconds>(system_clock::now().time_since_epoch());
     return impl::generateCode<SHA>(key, tm, digits, timeStep);
 }
 
 // Validate a time-based one-time password
 template <class SHA>
-bool validateCode(const std::string& code, const SecByteBlock& key,
-                  int digits = 6, seconds timeStep = seconds{30}) noexcept
-{
+auto validateCode(const std::string& code, const SecByteBlock& key, int digits = 6,
+                  seconds timeStep = seconds{30}) noexcept -> bool {
     return code == generateCode<SHA>(key, digits, timeStep);
 }
 
 // Seconds left before new time counter
-duration<double> timeLeft(seconds timeStep = seconds{30}) noexcept
-{
+auto timeLeft(seconds timeStep = seconds{30}) noexcept -> duration<double> {
     auto now = system_clock::now().time_since_epoch();
     return timeStep - duration<double>{now % timeStep};
 }
 
 // Generate current Google authenticator code
-std::optional<std::string>
-googleAuthenticatorCode(const std::string& base32Key) noexcept
-{
-    if (auto key = impl::googleAuthenticatorBase32Decode(base32Key))
-        return totp::generateCode<SHA1>(*key);
+auto googleAuthenticatorCode(const std::string& base32Key) noexcept -> std::optional<std::string> {
+    if (auto key = impl::googleAuthenticatorBase32Decode(base32Key)) return totp::generateCode<SHA1>(*key);
     return {};
 }
 
